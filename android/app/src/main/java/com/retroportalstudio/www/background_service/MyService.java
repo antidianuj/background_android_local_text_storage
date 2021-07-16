97c44625d287412c32bc32632b10e26ee009f57c
dianuj1997 <47445756+dianuj1997@users.noreply.github.com>
2021-07-17 04:20:28 +0900

    Add files via upload

--- android/app/src/main/java/com/retroportalstudio/www/background_service/MyService.java
+++ android/app/src/main/java/com/retroportalstudio/www/background_service/MyService.java
@@ -8,18 +8,49 @@ import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MyService extends Service {
+    private static final String FILE_NAME = "example.txt";
+
 
     @Override
     public void onCreate() {
+
         super.onCreate();
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            FileOutputStream fos = null;
+            try {
+                fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+                fos.write("Hello".getBytes());
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+
             NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"messages")
-                    .setContentText("This is running in Background")
+                    .setContentText(" ")
                     .setContentTitle("Flutter Background")
                     .setSmallIcon(R.drawable.ic_android_black_24dp);
+
 
             startForeground(101,builder.build());
         }
